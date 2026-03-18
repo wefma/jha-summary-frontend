75e7b3340d922adbd4785c09797ef71a301c70d7
wefma <wefmaika1985@gmail.com>
2026-03-18 12:35:34 +0000

    日本語のページとして認識させるようにした

--- app/app.vue
+++ app/app.vue
@@ -3,7 +3,7 @@ useHead({
   meta: [{ name: "viewport", content: "width=device-width, initial-scale=1" }],
   link: [{ rel: "icon", href: "/favicon.ico" }],
   htmlAttrs: {
-    lang: "en",
+    lang: "ja",
   },
 });
 
